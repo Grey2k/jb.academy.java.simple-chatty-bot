3cf8526f4553f5f861da2f07494179c1c9fc544e
Sergey Goppikov <grey2k@gmail.com>
2023-03-13 19:58:27 +0300

    4/5

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -16,8 +16,17 @@ public class SimpleBot {
         System.out.println("Let me guess your age.");
         System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
 
-        // reading all remainders
+        int rem3 = scanner.nextInt();
+        int rem5 = scanner.nextInt();
+        int rem7 = scanner.nextInt();
 
-        System.out.println("Your age is {yourAge}; that's a good time to start programming!");
+        int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+        System.out.println("Now I will prove to you that I can count to any number you want.");
+
+        // read a number and count to it here
+
+        System.out.println("Completed, have a nice day!");
     }
 }
--- Simple Chatty Bot/task/test/Tests.java
+++ Simple Chatty Bot/task/test/Tests.java
@@ -8,10 +8,12 @@ import java.util.List;
 class Clue {
     int age;
     String name;
+    int count;
 
-    Clue(String name, int age) {
+    Clue(String name, int age, int count) {
         this.age = age;
         this.name = name;
+        this.count = count;
     }
 }
 
@@ -22,12 +24,8 @@ public class Tests extends StageTest<Clue> {
     public List<TestCase<Clue>> generate() {
         return List.of(
             new TestCase<Clue>()
-                .setInput("John\n1\n2\n1")
-                .setAttach(new Clue("John", 22)),
-
-            new TestCase<Clue>()
-                .setInput("Nick\n2\n0\n0")
-                .setAttach(new Clue("Nick", 35))
+                .setInput("Marry\n1\n0\n5\n10")
+                .setAttach(new Clue("Marry", 40, 10))
         );
     }
 
@@ -36,9 +34,13 @@ public class Tests extends StageTest<Clue> {
 
         String[] lines = reply.trim().split("\n");
 
-        if (lines.length != 7) {
+        int length = 9 + clue.count + 1;
+
+        if (lines.length != length) {
             return CheckResult.wrong(
-                "You should output 7 lines. Lines found: " + lines.length + "\n" +
+                "You should output " + length + " lines " +
+                    "(for the count number " + clue.count +").\n" +
+                    "Lines found: " + lines.length + "\n" +
                     "Your output:\n" +
                     reply
             );
@@ -66,6 +68,20 @@ public class Tests extends StageTest<Clue> {
                     "Maybe you calculated the age wrong?\n\n" +
                     "Your last line: \n" + "\"" + lines[6] + "\""
             );
+        }
+
+        for (int i = 0; i < clue.count + 1; i++) {
+            String numLine = lines[i + 8];
+            String actualNum = i + "!";
+
+            if (!numLine.equals(actualNum)) {
+                return CheckResult.wrong(
+                    "Expected " + (i+8) + "-th line: \n" +
+                        "\"" + actualNum + "\"\n" +
+                        "Your "+ (i+8) + "-th line: \n" +
+                        "\"" + numLine + "\""
+                );
+            }
         }
 
         return CheckResult.correct();
